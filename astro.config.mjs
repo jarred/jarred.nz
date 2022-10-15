4fa627586164cf1ac0d4e7a4dd677e3a02d28cd9
jarred <jarred@hey.com>
2022-10-16 12:47:22 +1300

    add tailwind and tidy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import deno from "@astrojs/deno";
+import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
+  integrations: [prefetch(), tailwind()],
 });
