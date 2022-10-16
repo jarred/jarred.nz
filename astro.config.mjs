f40407a977a97223d7bff9ecd5eaf5ac9a977fda
jarred <jarred@hey.com>
2022-10-16 15:51:12 +1300

    try some images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,28 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import m2dx from "astro-m2dx";
+import remarkDirective from "remark-directive";
+
+/** @type {import('astro-m2dx').Options} */
+import image from "@astrojs/image";
+const m2dxOptions = {
+  styleDirectives: true
+};
+
+// https://astro.build/config
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [prefetch(), tailwind()],
-  adapter: vercel(),
+  integrations: [mdx(), tailwind(), prefetch(), image()],
+  markdown: {
+    remarkPlugins: [[m2dx, m2dxOptions]],
+    extendDefaultPlugins: true,
+    astroFlavoredMarkdown: false
+  },
+  adapter: vercel()
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const typography = require("@tailwindcss/typography");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      black: "#000",
+      white: "#FFF",
+      grey: "#EEE",
+    },
+    extend: {},
+  },
+  plugins: [typography],
+};
